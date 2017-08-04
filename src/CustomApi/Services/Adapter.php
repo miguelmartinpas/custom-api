fdfb6c8d40687386cff740254962782807882b5d
Miguel Martin <miguel.martin@piksel.com>
2017-08-04 21:23:41 +0200

    add GuzzleHttp module to laravel to call external API. Add first dump version in service

--- src/CustomApi/Services/Adapter.php
+++ src/CustomApi/Services/Adapter.php
@@ -3,12 +3,28 @@
 namespace CustomApi\Services;
 
 use CustomApi\Services\BaseService as BaseService;
+use GuzzleHttp\Client;
 
 class Adapter extends BaseService {
+
+  protected $url = "http://api.tvmaze.com/search/shows";
+  public $client;
+  public $result;
+
+  public function __construct(){
+    $this->client = new Client();
+  }
+
   public function get() {
+    $response = $this->client->request('GET', $this->url, [
+        'query' => [
+          'q' => 'deadpool'
+        ]
+    ]);
+
     return response()->json([
-      'message' => 'response from service for custom JSON API!',
-      'code' => '01'
+      'data' => json_decode($response->getBody()),
+      'status' => $response->getStatusCode()
     ]);
   }
 }
